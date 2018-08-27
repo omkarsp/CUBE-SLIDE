897eb327a46dffbd5aebd1b5c8ccd68e567353c9
omkarsp <omkarposhatwar12345@gmail.com>
2018-08-27 19:55:30 +0530

    first commit
    first  Github commit of CubeSlide 3D Unity game

---
+++ CUBE_SLIDE/Assets/scripts/playerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class playerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+     
+    void OnCollisionEnter(Collision collisioninfo)
+    {
+        if(collisioninfo.collider.tag == "obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
